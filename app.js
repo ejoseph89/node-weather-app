708e90dfbee0fa20900b4ff994392e1e9562867c
Emil Joseph <emiljoseph89@gmail.com>
2020-12-06 13:31:46 -0700

    added Geocoding using mapbox api

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
-const request = require('request');
-require('dotenv').config();
+//  REQUIRED MODULES
+const request = require('request')
+require('dotenv').config()
 
-/**
- * &key=value | eg: &units=f
- */
-const URL = process.env.URL;
+// Goal: Address -> Lat/Long -> Weather
 
-request({ url: URL, json: true }, (error, response) => {
+// WEATHERSTACK 
+const weatherURL = `http://api.weatherstack.com/current?${process.env.weatherKEY}&query=37.8267,-122.4233&units=f`;
 
-  console.log(`Conditions: ${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees, and feels like ${response.body.current.feelslike} degrees out.`)
+request({ url: weatherURL, json: true }, (error, response) => {
+  if (error) {
+    console.log('Unable to connect to weather service!')
+  } else if (response.body.error) {
+    console.log('Unable to find location')
+  } else {
+    console.log(`Conditions: ${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees, and feels like ${response.body.current.feelslike} degrees out.`)
+  }
+})
+
+// GEOCODING
+const mapboxURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?${process.env.mapboxKEY}`;
+
+request({ url: mapboxURL, json: true }, (error, response) => {
+  if (error) {
+    console.log('Unable to connect to location service!')
+  } else if (response.body.features.length === 0) {
+    console.log('Unable to find requested location')
+  } else {
+    const lattitude = response.body.features[0].geometry.coordinates[1];
+    const longitude = response.body.features[0].geometry.coordinates[0];
+    console.log(`Location: Los Angeles | Lat: ${lattitude}, Long: ${longitude}`)
+  }
 })
